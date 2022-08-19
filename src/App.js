93919c70cdbb5a158042db30a167cff7b4ff9966
HDYS-TTBYS <tatehide151@outlook.jp>
2022-08-19 22:56:56 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
+import { useEffect, useState } from "react";
 import './App.css';
 
 function App() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    // ここから
+    let randomString = []
+    const appendData = () => {
+      for (let i = 0; i < Math.floor(Math.random() * (15 + 1 - 10)) + 10; i++) { //Max:25 Min:10
+        randomString.push(Math.random().toString(32).substring(2))
+      }
+      // ここの処理をAPIからの取得のコードに変更する
+
+      setData([...data, ...randomString])
+
+      setTimeout(() => {
+        appendData()
+      }, 1000);
+    }
+    appendData()
+  }, []);
+
+  useEffect(() => {
+    data.slice(-1)[0] && document.getElementById(data.slice(-1)[0]).scrollIntoView({ behavior: "smooth" })
+  }, [data])
+
+  console.log(data.slice(-1)[0])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="scroll">
+      {data.map(d => <section key={d} id={d}>{d}</section >)}
     </div>
   );
 }
